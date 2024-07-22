c32c97eea4322438a56c0d0e060373ef358610fe
buğra uysal <uysalbugra134@gmail.com>
2024-07-22 03:41:52 +0300

    geri sayımı  entri ile girilen veriyle geri sayım yapmaya başladı

--- pmdrtmr.py
+++ pmdrtmr.py
@@ -11,14 +11,30 @@ def nowtime():
     """ güncel saat """
     watch=strftime('%H:%M:%S %p')
     süre.config(text=watch)
-    süre.after(1000,watch)
+    süre.after(1000,nowtime)
    
-   
+sgır=tk.Entry(yer, text="")
+sgır.place(x=10 , y=50)
 
+     
+
+
+
+
+def start():
+   try:
+      count=int(sgır.get())
+      countdown(count)
+      
+       
+
+   except ValueError:    
+      gerisyn["text"]="sayı giriniz"
+       
+  
 """ süreyi başlatma butonu """
-başlat=tk.Button(yer,text="başlat" , command=nowtime )
+başlat=tk.Button(yer,text="başlat" , command=start )
 başlat.place(relx=0.5,rely=0.5)
-
 
 """ güncel saat  """
 süre=tk.Label(yer,)
@@ -26,24 +42,29 @@ süre.place(relx=0.92 , rely=0.02 , anchor="center")
 """ geri sayım yeri """
 gerisyn=tk.Label(yer,)
 gerisyn.place(relx=0.7, rely=0.7, height=30, width=120)
-""" süre belirtin """
+""" süre belirtin 
 girsüre=tk.Entry(yer,text="" )
 girsüre.place(relx=0.3 , rely=0.5, height=30 , width=130)
+"""
 
+
+
+   
+     
+
+ 
 """ geri sayım """
 def countdown(count):
-    gerisyn["text"]=count
+  gerisyn["text"]=count
 
-    if count>0:
-        yer.after(1000, countdown , count-1)
-        rr=girsüre.get()
+  if count>0:
+     yer.after(1000,countdown  ,count-1)
 
-    else: 
-        gerisyn["text"]="süreniz doldu"   
+  else:
+     gerisyn["text"]="süreniz doldu"
 
-
-countdown(10)
 nowtime()
+
 yer.mainloop()
 
 
