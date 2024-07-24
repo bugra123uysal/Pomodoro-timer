7ed318c2198954c23dc36e73d0c1c4294b44c572
buğra uysal <uysalbugra134@gmail.com>
2024-07-24 17:13:40 +0300

    çalışma süresi ve mola süresi ve süre başlat buttono nu yerleri belirlendi
    geri sayım süreleri eklenecek

--- pmdrtmr.py
+++ pmdrtmr.py
@@ -7,63 +7,31 @@ import tkinter as tk
 yer=tk.Tk()
 yer.geometry("600x700")
 """ saat """
+sath=tk.Label(yer,)
+sath.place(x=525 , y=10 )
+
 def nowtime():
-    """ güncel saat """
     watch=strftime('%H:%M:%S %p')
-    süre.config(text=watch)
-    süre.after(1000,nowtime)
-   
-sgır=tk.Entry(yer, text="")
-sgır.place(x=10 , y=50)
-
-     
-
-
-
-
-def start():
-   try:
-      count=int(sgır.get())
-      countdown(count)
-      
-       
-
-   except ValueError:    
-      gerisyn["text"]="sayı giriniz"
-       
-  
-""" süreyi başlatma butonu """
-başlat=tk.Button(yer,text="başlat" , command=start )
-başlat.place(relx=0.5,rely=0.5)
-
-""" güncel saat  """
-süre=tk.Label(yer,)
-süre.place(relx=0.92 , rely=0.02 , anchor="center")
-""" geri sayım yeri """
-gerisyn=tk.Label(yer,)
-gerisyn.place(relx=0.7, rely=0.7, height=30, width=120)
-""" süre belirtin 
-girsüre=tk.Entry(yer,text="" )
-girsüre.place(relx=0.3 , rely=0.5, height=30 , width=130)
-"""
-
-
-
-   
-     
-
- 
-""" geri sayım """
-def countdown(count):
-  gerisyn["text"]=count
-
-  if count>0:
-     yer.after(1000,countdown  ,count-1)
-
-  else:
-     gerisyn["text"]="süreniz doldu"
+    sath.config(text=watch)
+    sath.after(1000, nowtime)
 
 nowtime()
+
+""" süre çalışma """
+
+
+workk=tk.Label(yer, bd=2, relief="solid")
+workk.place(x=10, y=100 , height=130, width=200)
+
+
+""" mola """
+mlo=tk.Label(yer, bd=2, relief="solid")
+mlo.place(x=390, y=100 , height=130, width=200)
+
+"""süreyi  başlat buttonu """
+
+bşltbtn=tk.Button(yer, text="başlat")
+bşltbtn.place(x=230 , y=330 , height=40, width=140)
 
 yer.mainloop()
 
