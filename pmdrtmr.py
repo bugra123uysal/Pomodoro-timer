4cdb4b83503ee9024c5e4cf444ba7107d3c5abf3
buğra uysal <uysalbugra134@gmail.com>
2024-07-25 04:51:46 +0300

    kullanıcıdan süreyi alıp çalışma süresini geri sayım yaptırdım
    mola süresi vermeyi yapacağım

--- pmdrtmr.py
+++ pmdrtmr.py
@@ -1,4 +1,5 @@
 from time import strftime
+import time
 import tkinter as tk
 
 
@@ -16,23 +17,44 @@ def nowtime():
     sath.after(1000, nowtime)
 
 nowtime()
+def bşlll():
+    time_over=int(gır.get())*60
+    count(time_over)
+    
+    
+
+def count(time_over):
+    if time_over >0:
+        mins,secs=divmod(time_over,60)
+        timeformer='{:02d}:{:02d}'.format(mins,secs)
+        work.config(text=timeformer)
+        work.after(1000,lambda:count(time_over-1) )
+       
+  
+    else:
+        work.config(text="süre doldu")
+        
 
 """ süre çalışma """
 
 
-workk=tk.Label(yer, bd=2, relief="solid")
-workk.place(x=10, y=100 , height=130, width=200)
+work=tk.Label(yer, bd=2, relief="solid")
+work.place(x=10, y=100 , height=130, width=200)
 
 
 """ mola """
 mlo=tk.Label(yer, bd=2, relief="solid")
 mlo.place(x=390, y=100 , height=130, width=200)
+""" süreyi gir """
+
+
 
 """süreyi  başlat buttonu """
-
-bşltbtn=tk.Button(yer, text="başlat")
+bşltbtn=tk.Button(yer, text="başlat", command=bşlll)
 bşltbtn.place(x=230 , y=330 , height=40, width=140)
-
+""" süre gir """
+gır=tk.Entry(yer,)
+gır.place(x=100,y=1, height=20, width=140)
 yer.mainloop()
 
 
